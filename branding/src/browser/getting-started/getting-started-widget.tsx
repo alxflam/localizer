268636442d879846425ae428616154d344e307ea
Alexander Flammer <alex.flammer.dev@outlook.com>
2021-08-05 18:59:15 +0200

    reformat getting started widget

--- branding/src/browser/getting-started/getting-started-widget.tsx
+++ branding/src/browser/getting-started/getting-started-widget.tsx
@@ -9,34 +9,34 @@ export class LocalizerGettingStartedWidget extends GettingStartedWidget {
      * Collection of useful links to display for end users.
      */
     protected readonly repositoryUrl = 'https://www.github.com/alxflam/localizer';
-   
+
     @postConstruct()
     protected async init(): Promise<void> {
         super.init();
         this.update();
     }
 
-        /**
+    /**
      * Render the help section.
      */
-         protected renderHelp(): React.ReactNode {
-            return <div className='gs-section'>
-                <h3 className='gs-section-header'>
-                    <i className='fa fa-question-circle'></i>
-                    Help
-                </h3>
-                <div className='gs-action-container'>
-                    <a
-                        role={'button'}
-                        tabIndex={0}
-                        onClick={() => this.doOpenExternalLink(this.repositoryUrl)}
-                        onKeyDown={(e: React.KeyboardEvent) => this.doOpenExternalLinkEnter(e, this.repositoryUrl)}>
-                        Source Code Repository
-                    </a>
-                </div>
-            </div>;
-        }
-    
+    protected renderHelp(): React.ReactNode {
+        return <div className='gs-section'>
+            <h3 className='gs-section-header'>
+                <i className='fa fa-question-circle'></i>
+                Help
+            </h3>
+            <div className='gs-action-container'>
+                <a
+                    role={'button'}
+                    tabIndex={0}
+                    onClick={() => this.doOpenExternalLink(this.repositoryUrl)}
+                    onKeyDown={(e: React.KeyboardEvent) => this.doOpenExternalLinkEnter(e, this.repositoryUrl)}>
+                    Source Code Repository
+                </a>
+            </div>
+        </div>;
+    }
 
-    
+
+
 }
